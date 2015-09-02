0bbf536c30349f433b10d15dccb44234566d4cf0
Rabin Julien <julienr114@gmail.com>
2015-09-02 10:53:25 +0200

    header.php
    création du fichier leader.php

---
+++ header.php
@@ -0,0 +1,9 @@
+<header class="row">
+    <div class="col-md-10">
+        </br>
+        <img src="img/logo.png">
+    </div>
+    <div class="col-md-2">
+        <img src="img/banniere.png" height="230px">
+    </div>
+</header>
