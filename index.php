ce200c74498f50700d72cc8871e20c024d822238
Laurent Berthier <proberthier@gmail.com>
2015-09-02 11:20:50 +0200

    creation du php pour menu-image

--- index.php
+++ index.php
@@ -27,67 +27,9 @@
                 </div>
             </div>
 
-            <div class="menu-image">
-                <div class="row">
-                    <div class="col-md-1"></div>
-                    <div class="col-md-2">
-                        <div class="thumbnail" title="Come share my healthy breakfast">
-                            <a id="kfc" href="samedi/kfc.html"><img src="img/kfc_thumbnail.jpg" alt="Come share my healthy breakfeast"></a>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="thumbnail" title="Take a walk with my dog">
-                            <a id="dog" href="samedi/dog.html"><img src="img/dog_thumbnail.jpg" alt="Take a walk with my dog"></a>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="thumbnail" title="Would you play game of drone with me?">
-                            <a id="game" href="samedi/game.html"><img src="img/game_thumbnail.jpg" alt="Would you play game of drone with me?"></a>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="thumbnail" title="Shoot the terrorist!">
-                            <a id="balltrap" href="samedi/balltrap.html"><img src="img/balltrap_thumbnail.jpg" alt="Shoot the terrorist!"></a>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="thumbnail" title="Time to chill out in the Jacuzzi with Michelle and me">
-                            <a id="jacuzzi" href="samedi/jacuzzi.html"><img src="img/jacuzzi_thumbnail.jpg" alt="Time to chill out in the Jacuzzi with Michelle and me"></a>
-                        </div>
-                    </div>
-                    <div class="col-md-1"></div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-1"></div>
-                    <div class="col-md-2">
-                        <div class="thumbnail" title="Let's pray the lord">
-                            <a id="gospel" href="dimanche/gospel.html"><img src="img/gospel_thumbnail.jpg" alt="Let's pray the lord!"></a>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="thumbnail">
-                            <a id="meal" href="dimanche/meal.html"><img src="img/balltrap_thumbnail.jpg"></a>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="thumbnail" title="Let's do some prank to Putine">
-                            <a id="phone" href="dimanche/phone.html"><img src="img/phone_thumbnail.jpg" alt="Let's do somme prank to Putine"></a>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="thumbnail" title="Test the white house private basket ball room">
-                            <a id="basketball" href="dimanche/basketball.html"><img src="img/basketball_thumbnail.jpg" alt="Test the white house private basket ball room"></a>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="thumbnail">
-                            <a id="tv" href="dimanche/tv.html"><img src="img/jacuzzi_thumbnail.jpg"></a>
-                        </div>
-                    </div>
-                    <div class="col-md-1"></div>
-                </div>
-            </div>
+            <?php
+              include("menu_image.php");
+            ?>
 
             <div class="row">
                 <div class="col-md-12">
