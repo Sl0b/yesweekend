bbb7a156be504e1f93c99c0adbcdf642ffbc2e87
Laurent Berthier <proberthier@gmail.com>
2015-09-03 09:04:28 +0200

    test sur le header

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
-<header class="row">
-    <div class="col-md-10">
-        </br>
-        <img src="img/logo.png">
-    </div>
-    <div class="col-md-2">
-        <img src="img/banniere.png" height="230px">
-    </div>
-</header>
+<div id="header__background">
+    <header class="row">
+        <div class="col-md-10">
+            </br>
+            <img src="img/logo.png">
+        </div>
+        <div class="col-md-2">
+            <img src="img/banniere.png" height="230px">
+        </div>
+    </header>
+</div>
