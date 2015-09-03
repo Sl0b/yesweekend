2e829756a126b64ab810be4cfce06f9b925f613f
Laurent Berthier <proberthier@gmail.com>
2015-09-03 17:14:19 +0200

    article, liens vers les pages, finition sur thumbnails

---
+++ game.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>YESWEEKEND</title>
+        <meta name="description" content="Obama's life">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+
+        <div class="container">
+
+            <?php
+              include("header.php")
+            ?>
+              <div class="row font-text">
+                  <img src="img/smile2.jpg" class="img-circle col-md-4">
+                  <div class="col-md-8">
+                    <p class="text2">
+                    Hello guys !</br></br>
+                    Maybe you recognize me. I'm Barack and I'm not just the president of the United States of America, I'm a cool man too.
+                  </br>To prove it I show you my favorite activities of the weekend when I take a rest of being the most powerful man in the world. And if you're lucky your can win a weekend with me to the White House.
+                    </p>
+                  </div>
+              </div>
+
+
+            <?php
+              include("menu_image.php");
+            ?>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <article>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis pretium malesuada. Cras pretium nulla dolor, sit amet aliquam urna volutpat vitae. Donec eget odio imperdiet, sodales augue et, ornare ligula. Integer sollicitudin, lacus et gravida ultrices, arcu dolor tristique felis, vel accumsan ante neque sed dolor. Nullam dapibus justo quis urna rutrum rhoncus. Aliquam vestibulum ultrices nunc in hendrerit. Cras sed massa convallis, luctus massa in, euismod ante. Mauris lobortis sit amet nunc dictum volutpat. Proin porta mollis massa quis elementum. Proin placerat aliquet vulputate. Etiam consequat sodales fringilla. Vivamus tempus neque mi, a rutrum magna facilisis vel. In cursus at justo id tincidunt. Sed tellus turpis, convallis sit amet lectus non, pellentesque fringilla ante. Proin volutpat dui risus, nec tristique nisi iaculis at. Aenean ligula odio, pellentesque sit amet sodales vitae, pretium vel ligula.
+                        </p>
+                    </article>
+                </div>
+            </div>
+
+            <?php
+              include("footer.php")
+            ?>
+
+        </div>
+
+
+
+        <script type="text/javascript" src="js/history.js"></script>
+
+
+
+    </body>
+
+</html>
--- menu_image.php
+++ menu_image.php
@@ -3,32 +3,32 @@
         <div class="col-md-1"></div>
         <div class="col-md-2">
             <div class="thumbnail" title="Come share my healthy breakfast">
-                <a id="kfc" href="samedi/kfc.html"><img src="img/kfc_thumbnail.jpg" alt="Come share my healthy breakfeast"></a>
-                <p class="hover-test">KFC</p>
+                <img src="img/kfc_thumbnail.jpg" alt="Come share my healthy breakfeast">
+                <a id="kfc" href="kfc.php"><p class="hover-test">KFC</p></a>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail" title="Take a walk with my dog">
-                <a id="dog" href="samedi/dog.html"><img src="img/dog_thumbnail.jpg" alt="Take a walk with my dog"></a>
-                <p class="hover-test">Walking</p>
+                <img src="img/dog_thumbnail.jpg" alt="Take a walk with my dog">
+                <a id="dog" href="dog.php"><p class="hover-test">Walking</p></a>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail" title="Would you play game of drone with me?">
-                <a id="game" href="samedi/game.html"><img src="img/game_thumbnail.jpg" alt="Would you play game of drone with me?"></a>
-                <p class="hover-test">Video&nbsp;Games</p>
+                <img src="img/game_thumbnail.jpg" alt="Would you play game of drone with me?">
+                <a id="game" href="game.php"><p class="hover-test">Video&nbsp;Games</p></a>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail" title="Shoot the terrorist!">
-                <a id="balltrap" href="samedi/balltrap.html"><img src="img/balltrap_thumbnail.jpg" alt="Shoot the terrorist!"></a>
-                <p class="hover-test">Balltrap</p>
+                <img src="img/balltrap_thumbnail.jpg" alt="Shoot the terrorist!">
+                <a id="balltrap" href="balltrap.php"><p class="hover-test">Balltrap</p></a>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail" title="Time to chill out in the Jacuzzi with Michelle and me">
-                <a id="jacuzzi" href="samedi/jacuzzi.html"><img src="img/jacuzzi_thumbnail.jpg" alt="Time to chill out in the Jacuzzi with Michelle and me"></a>
-                <p class="hover-test">Jacuzzi</p>
+                <img src="img/jacuzzi_thumbnail.jpg" alt="Time to chill out in the Jacuzzi with Michelle and me">
+                <a id="jacuzzi" href="jacuzzi.php"><p class="hover-test">Jacuzzi</p></a>
             </div>
         </div>
         <div class="col-md-1"></div>
@@ -38,32 +38,32 @@
         <div class="col-md-1"></div>
         <div class="col-md-2">
             <div class="thumbnail" title="Let's pray the lord">
-                <a id="gospel" href="dimanche/gospel.html"><img src="img/gospel_thumbnail.jpg" alt="Let's pray the lord!"></a>
-                <p class="hover-test">Gospel</p>
+                <img src="img/gospel_thumbnail.jpg" alt="Let's pray the lord!">
+                <a id="gospel" href="gospel.php"><p class="hover-test">Gospel</p></a>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail">
-                <a id="meal" href="dimanche/meal.html"><img src="img/balltrap_thumbnail.jpg"></a>
-                <p class="hover-test">Familly</p>
+                <img src="img/meal-thumbnail.jpg">
+                <a id="meal" href="meal.php"><p class="hover-test">Familly</p></a>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail" title="Let's do some prank to Putine">
-                <a id="phone" href="dimanche/phone.html"><img src="img/phone_thumbnail.jpg" alt="Let's do somme prank to Putine"></a>
-                <p class="hover-test">Having Fun</p>
+                <img src="img/phone_thumbnail.jpg" alt="Let's do somme prank to Putine">
+                <a id="phone" href="phone.php"><p class="hover-test">Having Fun</p></a>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail" title="Test the white house private basket ball room">
-                <a id="basketball" href="dimanche/basketball.html"><img src="img/basketball_thumbnail.jpg" alt="Test the white house private basket ball room"></a>
-                <p class="hover-test">Baskett-ball</p>
+                <img src="img/basketball_thumbnail.jpg" alt="Test the white house private basket ball room">
+                <a id="basketball" href="basketball.php"><p class="hover-test">Baskett-ball</p></a>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail">
-                <a id="tv" href="dimanche/tv.html"><img src="img/jacuzzi_thumbnail.jpg"></a>
-                <p class="hover-test">TV Show</p>
+                <img src="img/rsz_obamatv.jpg">
+                <a id="tv" href="tv.php"><p class="hover-test">TV Show</p></a>
             </div>
         </div>
         <div class="col-md-1"></div>
