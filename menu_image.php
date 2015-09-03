7b1112473dc7473d4e3e61a3ea0a715a220e3c4f
Laurent Berthier <proberthier@gmail.com>
2015-09-03 16:19:22 +0200

    finition sur menu-image

--- menu_image.php
+++ menu_image.php
@@ -10,21 +10,25 @@
         <div class="col-md-2">
             <div class="thumbnail" title="Take a walk with my dog">
                 <a id="dog" href="samedi/dog.html"><img src="img/dog_thumbnail.jpg" alt="Take a walk with my dog"></a>
+                <p class="hover-test">Walking</p>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail" title="Would you play game of drone with me?">
                 <a id="game" href="samedi/game.html"><img src="img/game_thumbnail.jpg" alt="Would you play game of drone with me?"></a>
+                <p class="hover-test">Video&nbsp;Games</p>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail" title="Shoot the terrorist!">
                 <a id="balltrap" href="samedi/balltrap.html"><img src="img/balltrap_thumbnail.jpg" alt="Shoot the terrorist!"></a>
+                <p class="hover-test">Balltrap</p>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail" title="Time to chill out in the Jacuzzi with Michelle and me">
                 <a id="jacuzzi" href="samedi/jacuzzi.html"><img src="img/jacuzzi_thumbnail.jpg" alt="Time to chill out in the Jacuzzi with Michelle and me"></a>
+                <p class="hover-test">Jacuzzi</p>
             </div>
         </div>
         <div class="col-md-1"></div>
@@ -35,26 +39,31 @@
         <div class="col-md-2">
             <div class="thumbnail" title="Let's pray the lord">
                 <a id="gospel" href="dimanche/gospel.html"><img src="img/gospel_thumbnail.jpg" alt="Let's pray the lord!"></a>
+                <p class="hover-test">Gospel</p>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail">
                 <a id="meal" href="dimanche/meal.html"><img src="img/balltrap_thumbnail.jpg"></a>
+                <p class="hover-test">Familly</p>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail" title="Let's do some prank to Putine">
                 <a id="phone" href="dimanche/phone.html"><img src="img/phone_thumbnail.jpg" alt="Let's do somme prank to Putine"></a>
+                <p class="hover-test">Having Fun</p>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail" title="Test the white house private basket ball room">
                 <a id="basketball" href="dimanche/basketball.html"><img src="img/basketball_thumbnail.jpg" alt="Test the white house private basket ball room"></a>
+                <p class="hover-test">Baskett-ball</p>
             </div>
         </div>
         <div class="col-md-2">
             <div class="thumbnail">
                 <a id="tv" href="dimanche/tv.html"><img src="img/jacuzzi_thumbnail.jpg"></a>
+                <p class="hover-test">TV Show</p>
             </div>
         </div>
         <div class="col-md-1"></div>
